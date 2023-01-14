e20644b99896b48ed8c06d6b01b454de2850cff4
Akvarion <71176592+Akvarion@users.noreply.github.com>
2023-01-14 16:37:06 +0100

    Update client.c

--- src/client.c
+++ src/client.c
@@ -2,14 +2,18 @@
  *    Akvarion, Jan 2023
 */
 
-// #include "../odpi-main/include/dpi.h" //PATH TO ODPI-C COMPILED FOLDER
+#include "../odpi-main/include/dpi.h" //PATH TO ODPI-C
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
-#define CONN_STRING "172.17.0.2:1521/XEPDB1"
+#define FALSE 0
+#define TRUE 1
+#define DEBUG_USER_INPUT 0
+#define CONN_STRING "172.17.0.2:1521/XEPDB1"    // this mf took 2h of research to find
 
-//Funzione per mostrare a video errori nell'istanziazione del contesto
+//prints errors (duh)
 void printError(dpiContext *context, dpiErrorInfo *ErrorInfo){
     dpiContext_getError(context, ErrorInfo);
     fprintf(stderr,"-----------------------------------------\n");
@@ -20,28 +24,76 @@ void printError(dpiContext *context, dpiErrorInfo *ErrorInfo){
     fflush(stderr);
 }
 
+//take a string out from the buffer and copy it in a string.
+int buff_out(char *dest, char *buffer, size_t *dest_size){
+    int i;
+    bool FLAG=FALSE;
+    //foreach charcter in the string
+    for(i=0;i<strlen(buffer)&&!FLAG;i++){
+        //check if this is the terminator and set the FLAG
+        if(buffer[i]=='\0'){
+           FLAG=TRUE;
+        }
+        //if the size of destination is smaller than what we need to copy, reallocate memory
+        if(*dest_size==(size_t)i){
+            dest=(char*)realloc(dest,(*dest_size+1)*sizeof(char));
+        }
+        dest[i]=buffer[i];
+    }
+    if(dest[i]=='\0'){
+        fprintf(stdout,"string has a null terminator");
+        return 0;
+    }
+    else {
+        fprintf(stdout, "string does not have a null terminator\n");
+        return -1;
+    }
 
-void get_user_info(const char *username, const char *password){
-    printf("-----------------------------------------\n");
-    printf("Username: ");
-    scanf("%s", username);
-    printf("\nPassword: ");
-    scanf("%s", password);
+}
+
+//User authentication dummy routine
+void get_user_info(char *username, char *password){
+    size_t size=0;
+    char buffer[32];
+    fprintf(stdout,"-----------------------------------------\n");
+    fprintf(stdout,"Username: ");
+
+    size=(size_t)fscanf(stdin,"%s", buffer);
+    username=(char*)malloc((size+1)*sizeof(char));
+    //strcpy(username,buffer);
+    size++;
+    buff_out(username,buffer,&size);
+    
+    fprintf(stdout,"\nPassword: ");
+    
+    size=(size_t)fscanf(stdin,"%s", buffer);
+    password=(char*)malloc((size+1)*sizeof(char));
+    //strcpy(password,buffer);
+    size++;
+    buff_out(password,buffer,&size);
+    
+
+    fflush(stdout);
+    fflush(stdin);
 }
 
 int main(int argc, char** argv){
-    dpiData intColValue, stringColValue;
-    uint32_t rowidStrLength;
-    const char *rowidStr;
-    uint64_t rowCount;
-    dpiRowid *rowid;
+    char *userName=NULL;
+    char *password=NULL;
     dpiStmt *statement;
     dpiConn *conn;
     dpiContext *context=NULL;
     dpiErrorInfo errorInfo;
-    uint32_t userNameLength;
-    const char *userName;
-    const char *password;
+
+    //GET USER CREDENTIALS
+    if(DEBUG_USER_INPUT){
+        //strings obtained in this way do not work, although they are nul terminated strings, so... uhm... skip.
+        get_user_info(userName,password);
+    }
+    else{
+        userName="ODINPARTS";
+        password="odinparts";
+    }
 
     //Creazione contesto per la connessione
     if(dpiContext_createWithParams(DPI_MAJOR_VERSION, DPI_MINOR_VERSION, NULL,&context,&errorInfo)!=DPI_SUCCESS){
@@ -51,10 +103,6 @@ int main(int argc, char** argv){
 
     printf("[CONTEXT CREATED SUCCESSFULLY]\n");
 
-    //GET USER CREDENTIALS, TODO
-    //get_user_info(userName,password);
-    userName="ODINPARTS";
-    password="odinparts";
     //CONNECTION CREATION
     if(dpiConn_create(context, userName, strlen(userName), password, strlen(password), CONN_STRING, strlen(CONN_STRING),NULL,NULL,&conn)!=DPI_SUCCESS){
         printError(context,&errorInfo);
@@ -69,6 +117,6 @@ int main(int argc, char** argv){
     //DESTROYING CONTEXT
     dpiContext_destroy(context);
 
-    printf("[DONE]");
+    printf("[DONE]\n");
     return 0;
 }
