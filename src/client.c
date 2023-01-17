aedae67bcc5a9747a71c005871903b736d4b2d8d
Akvarion <71176592+Akvarion@users.noreply.github.com>
2023-01-17 17:56:57 +0100

    added menu skeleton to handle different tasks

--- src/client.c
+++ src/client.c
@@ -7,11 +7,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <ctype.h>
 
 #define FALSE 0
 #define TRUE 1
 #define DEBUG_USER_INPUT 0
 #define CONN_STRING "172.17.0.2:1521/XEPDB1"    // this mf took 2h of research to find
+
+static dpiErrorInfo error_info;
 
 //prints errors (duh)
 void printError(dpiContext *context, dpiErrorInfo *ErrorInfo){
@@ -24,7 +27,7 @@ void printError(dpiContext *context, dpiErrorInfo *ErrorInfo){
     fflush(stderr);
 }
 
-//take a string out from the buffer and copy it in a string.
+//Not used.take a string out from the buffer and copy it in a string.
 int buff_out(char *dest, char *buffer, size_t *dest_size){
     int i;
     bool FLAG=FALSE;
@@ -52,42 +55,54 @@ int buff_out(char *dest, char *buffer, size_t *dest_size){
 }
 
 //User authentication dummy routine
+//REMEMBER TO FREE THE MEMORY
 void get_user_info(char *username, char *password){
     size_t size=0;
     char buffer[32];
+    memset(buffer,48,sizeof(buffer));
     fprintf(stdout,"-----------------------------------------\n");
     fprintf(stdout,"Username: ");
+    fflush(stdout);
 
-    size=(size_t)fscanf(stdin,"%s", buffer);
-    username=(char*)malloc((size+1)*sizeof(char));
-    //strcpy(username,buffer);
-    size++;
-    buff_out(username,buffer,&size);
-    
+    if ( fgets(buffer, sizeof(buffer), stdin) != NULL ){
+        char *newline = strchr(buffer, '\n'); /* search for newline character */
+        if ( newline != NULL ){
+            *newline = '\0'; /* overwrite trailing newline */
+        }
+    }
+    username=(char*)malloc((strlen(buffer)+1)*sizeof(char));
+    strcpy(username,buffer);
+    //size++;
+    //buff_out(username,buffer,&size);
+    memset(buffer,48,sizeof(buffer));
+
     fprintf(stdout,"\nPassword: ");
-    
-    size=(size_t)fscanf(stdin,"%s", buffer);
-    password=(char*)malloc((size+1)*sizeof(char));
-    //strcpy(password,buffer);
-    size++;
-    buff_out(password,buffer,&size);
+    fflush(stdout);
+
+    if (fgets(buffer, sizeof(buffer), stdin) != NULL ){
+        char *newline = strchr(buffer, '\n'); /* search for newline character */
+        if ( newline != NULL ){
+            *newline = '\0'; /* overwrite trailing newline */
+        }
+    }
+
+    password=(char*)malloc((strlen(buffer)+1)*sizeof(char));
+    strcpy(password,buffer);
+    //size++;
+    //buff_out(password,buffer,&size);
     
 
     fflush(stdout);
     fflush(stdin);
+
 }
 
-int main(int argc, char** argv){
+int conn_setup(dpiConn *conn, dpiContext *context){
     char *userName=NULL;
     char *password=NULL;
-    dpiStmt *statement;
-    dpiConn *conn;
-    dpiContext *context=NULL;
-    dpiErrorInfo errorInfo;
 
-    //GET USER CREDENTIALS
     if(DEBUG_USER_INPUT){
-        //strings obtained in this way do not work, although they are nul terminated strings, so... uhm... skip.
+        //Doesn't work, don't know why, throws segmentation fault. Maybe strings format or encoding?
         get_user_info(userName,password);
     }
     else{
@@ -95,23 +110,110 @@ int main(int argc, char** argv){
         password="odinparts";
     }
 
-    //Creazione contesto per la connessione
-    if(dpiContext_createWithParams(DPI_MAJOR_VERSION, DPI_MINOR_VERSION, NULL,&context,&errorInfo)!=DPI_SUCCESS){
-        printError(context,&errorInfo);
+    if(dpiContext_createWithParams(DPI_MAJOR_VERSION, DPI_MINOR_VERSION, NULL,&context,&error_info)!=DPI_SUCCESS){
+        printError(context,&error_info);
         return EXIT_FAILURE;
     }
-
     printf("[CONTEXT CREATED SUCCESSFULLY]\n");
 
     //CONNECTION CREATION
     if(dpiConn_create(context, userName, strlen(userName), password, strlen(password), CONN_STRING, strlen(CONN_STRING),NULL,NULL,&conn)!=DPI_SUCCESS){
-        printError(context,&errorInfo);
+        printError(context,&error_info);
         return EXIT_FAILURE;
     }
     printf("[CONNECTION CREATED SUCCESSFULLY]\n");
+    
+    return 0;
+}
+
+int create_table(dpiConn *conn){
+    int choice=-1;
+    fprintf(stdout,"____________________\n");
+    fprintf(stdout,"|| Table creation ||\n");
+    fprintf(stdout,"‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\n");
+    fprintf(stdout,"Do you wish to write SQL code now (1) or fetch from file (2)? [Enter 0 to abort].\n");
+    fflush(stdout);
+    fscanf(stdin,"%d", &choice);
+
+    if(choice==1){
+        char buffer[64], *user_statement=malloc(sizeof(char));
+        user_statement[0]='\0';
+        memset(buffer,48,sizeof(buffer));
+        bool FLAG=TRUE;
+        int size=0;
+        
+        fprintf(stdout,"Insert your SQL statement. Enter no characters to end the input.\n");
+        fflush(stdout);
+        //consuming buffer newline, because fscanf and fgets differences.
+        while ((getchar()) != '\n');
+        while(FLAG){
+            if(fgets(buffer,(int)sizeof(buffer),stdin)==NULL){
+                perror("Input Error\n");
+                break;;
+            }
+            else{
+                if(buffer[0]=='\n'){
+                    FLAG=FALSE;
+                }
+                else{
+                    size=size+strlen(buffer)+1;
+                    user_statement=realloc(user_statement,size*sizeof(char));
+                    strcat(user_statement,buffer);
+                }
+            }
+        }
+
+        fprintf(stdout,"You have written the following:\n\n%s",user_statement);
+
+    }
+    else if(choice==2){
+    }
+    else{
+        fprintf(stdout,"[ABORTING]...");
+        return 1;
+    }
+    return 0;
+}
+
+int main(int argc, char** argv){
+    int choice =-1;
+    dpiStmt *statement;
+    dpiConn *conn;
+    dpiContext *context=NULL;
+    //dpiErrorInfo errorInfo;
+    
+    if(conn_setup(conn, context)!=0){
+        perror("Errors occurred while connecting to DB instance.\n");
+        return EXIT_FAILURE;
+    }
+
+    while(choice!=0){
+        fprintf(stdout,"___________________________________\n");
+        fprintf(stdout,"|| Select a task to be executed: ||\n");
+        fprintf(stdout,"‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\n");
+        fprintf(stdout,"\t1. Create table\n");
+        fprintf(stdout,"\t2. Insert rows\n");
+        fprintf(stdout,"\t3. Select some rows\n");
+        fprintf(stdout,"\t4. Scroll rows from table\n");
+        fprintf(stdout,"\t0. Exit\n");
+        fprintf(stdout,"~: ");
+        fflush(stdout);
+
+        fscanf(stdin,"%d",&choice);
+        if(choice==1){
+            if(create_table(conn)){
+                perror("Unable to Create Table.");
+            }
+        }
+
+        if(choice==2){}
+        if(choice==3){}
+        if(choice==4){}
+
+    }
 
 
-    printf("[CLEANING UP..]\n");
+    printf("[CLEANING UP...]\n");
     //CLOSING CONNECTION
     dpiConn_release(conn);
     //DESTROYING CONTEXT
