2fdf88c1a1f366a05efaa355e0677121365c0db7
Akvarion <newplayerone@linuxmail.org>
2023-01-27 18:34:16 +0100

    Added file output to query result

--- src/client.c
+++ src/client.c
@@ -1,6 +1,6 @@
 /*
  *    Akvarion, Jan 2023
-*/
+ */
 
 #include "../odpi-main/include/dpi.h" //PATH TO ODPI-C
 #include <stdio.h>
@@ -10,6 +10,7 @@
 #include <ctype.h>
 #include <stdint.h>
 #include <inttypes.h>
+#include <time.h>
 
 #define FALSE 0
 #define TRUE 1
@@ -76,11 +77,12 @@ void get_user_info(char *username, char *password){
 
 /*
  *      Remove given section from string. Negative len means remove
- *      everything up to the end. Credits to M Ohem @ stackoverflow
+ *      everything up to the end.
+ *      Begin position is preserved. DATE->DAT if begin == 3
+ *      Credits to M Ohem @ stackoverflow
  *      
  */
-int str_cut(char *str, int begin, int len)
-{
+int str_cut(char *str, int begin, int len){
     int l = strlen(str);
 
     if (len < 0) len = l - begin;
@@ -107,7 +109,7 @@ int examining_data(dpiStmt *statement, dpiConn *conn, dpiContext *context, dpiDa
 }
 
 //Routine to execute the SQL_string query
-int statement_exec_routine(dpiConn *conn, dpiContext *context, char *SQL_string){
+int statement_exec_routine(dpiConn *conn, dpiContext *context, char *SQL_string, bool FILE_FLAG, FILE *fp){
     dpiStmt *statement;
     dpiData *col_value;
     dpiVar *var;
@@ -150,61 +152,121 @@ int statement_exec_routine(dpiConn *conn, dpiContext *context, char *SQL_string)
     if (dpiStmt_fetch(statement, &found, &bufferRowIndex) < 0){
         printError(context,&error_info);
         return -1;
-        }
-    else printf("fetch was successful.\n");    
+        } 
    
-   // printf("Columns retrieved: %d\n", (int)numQueryColumns);
-    printf("[FETCH RESULTS]\n");
-    fprintf(stdout,"‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\n");
-    //To do: loop to display db stdout response messages and rows.
+   // differentiate between queries and non-queries. Prepare output.
+    if(found){
+        if(FILE_FLAG){
+            fprintf(fp,"[FETCH RESULTS]\n");
+            fprintf(fp,"‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\n");
+            for(int i=1;i<=(int)numQueryColumns;i++){
+                if(dpiStmt_getQueryInfo(statement,i,&info)!=DPI_SUCCESS){
+                    printError(context,&error_info);
+                    return -1;
+                }
+                fprintf(fp,"%s\t",info.name);
+            }
+            fprintf(fp,"\n");
+        }
+        else{
+            fprintf(stdout,"[FETCH RESULTS]\n");
+            fprintf(stdout,"‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\n");
+            for(int i=1;i<=(int)numQueryColumns;i++){
+                if(dpiStmt_getQueryInfo(statement,i,&info)!=DPI_SUCCESS){
+                    printError(context,&error_info);
+                    return -1;
+                }
+                fprintf(stdout,"%s\t",info.name);
+            }
+            fprintf(stdout,"\n");
+        }
+    }
 
     while (found==1){
         //populating metadata structure
-        char buffer[64];
         //printing query result
         for(int i=1;i<=(int)numQueryColumns;i++){
             if(dpiStmt_getQueryInfo(statement,i,&info)!=DPI_SUCCESS){
                 printError(context,&error_info);
                 return -1;
             }
+            
+            //getting values
             if(dpiStmt_getQueryValue(statement,i,&info.typeInfo.defaultNativeTypeNum, &col_value)){
                 printError(context,&error_info);
                 return -1;
             }
-            //check su col_value per stamparli in modo corretto
-            switch (info.typeInfo.defaultNativeTypeNum){
-                case DPI_NATIVE_TYPE_BOOLEAN:
-                    fprintf(stdout,"%d\t",(int)col_value->value.asBoolean);
-                    break;
-                case DPI_NATIVE_TYPE_BYTES:
-                    fprintf(stdout,"%.*s\t",col_value->value.asBytes.length,col_value->value.asBytes.ptr);
-                    break;
-                case DPI_NATIVE_TYPE_DOUBLE:
-                    fprintf(stdout,"%.2f\t",col_value->value.asDouble);
-                    break;
-                case DPI_NATIVE_TYPE_FLOAT:
-                    fprintf(stdout,"%.2f\t",col_value->value.asFloat);
-                    break;    
-                case DPI_NATIVE_TYPE_INT64:
-                    fprintf(stdout,"%" PRIx64 "\t",col_value->value.asInt64);
-                    break;
-                case DPI_NATIVE_TYPE_INTERVAL_DS:
-                    fprintf(stdout,"%" PRIx32 "|""%" PRIx32 "|""%" PRIx32 "|""%" PRIx32 "|""%" PRIx32 "\t",col_value->value.asIntervalDS.days,col_value->value.asIntervalDS.hours,col_value->value.asIntervalDS.minutes,col_value->value.asIntervalDS.seconds,col_value->value.asIntervalDS.fseconds);
-                    break;
-                case DPI_NATIVE_TYPE_INTERVAL_YM:
-                    fprintf(stdout,"%" PRIx32 "|""%" PRIx32 "\t",col_value->value.asIntervalYM.months,col_value->value.asIntervalYM.years);
-                    break;
-                case DPI_NATIVE_TYPE_NULL:
-                    break;
-                case DPI_NATIVE_TYPE_TIMESTAMP:
-                    fprintf(stdout,"%" PRIu8 "|""%" PRIu8 "|""%d\t",col_value->value.asTimestamp.day,col_value->value.asTimestamp.month,col_value->value.asTimestamp.year);
-                    break;     
-                default:
-                    break;
-            }
+            //check datatype to print it correctly. Differientiate between file or stdout
+            if(FILE_FLAG){
+                switch (info.typeInfo.defaultNativeTypeNum){
+                    case DPI_NATIVE_TYPE_BOOLEAN:
+                        fprintf(fp,"%d\t",(int)col_value->value.asBoolean);
+                        break;
+                    case DPI_NATIVE_TYPE_BYTES:
+                        fprintf(fp,"%.*s\t",col_value->value.asBytes.length,col_value->value.asBytes.ptr);
+                        break;
+                    case DPI_NATIVE_TYPE_DOUBLE:
+                        fprintf(fp,"%.2f\t",col_value->value.asDouble);
+                        break;
+                    case DPI_NATIVE_TYPE_FLOAT:
+                        fprintf(fp,"%.2f\t",col_value->value.asFloat);
+                        break;    
+                    case DPI_NATIVE_TYPE_INT64:
+                        fprintf(fp,"%" PRIx64 "\t",col_value->value.asInt64);
+                        break;
+                    case DPI_NATIVE_TYPE_INTERVAL_DS:
+                        fprintf(fp,"%" PRIx32 "|""%" PRIx32 "|""%" PRIx32 "|""%" PRIx32 "|""%" PRIx32 "\t",col_value->value.asIntervalDS.days,col_value->value.asIntervalDS.hours,col_value->value.asIntervalDS.minutes,col_value->value.asIntervalDS.seconds,col_value->value.asIntervalDS.fseconds);
+                        break;
+                    case DPI_NATIVE_TYPE_INTERVAL_YM:
+                        fprintf(fp,"%" PRIx32 "|""%" PRIx32 "\t",col_value->value.asIntervalYM.months,col_value->value.asIntervalYM.years);
+                        break;
+                    case DPI_NATIVE_TYPE_NULL:
+                        break;
+                    case DPI_NATIVE_TYPE_TIMESTAMP:
+                        fprintf(fp,"%" PRIu8 "|""%" PRIu8 "|""%d\t",col_value->value.asTimestamp.day,col_value->value.asTimestamp.month,col_value->value.asTimestamp.year);
+                        break;     
+                    default:
+                        break;
+                }
             
+            }
+            else{
+                switch (info.typeInfo.defaultNativeTypeNum){
+                    case DPI_NATIVE_TYPE_BOOLEAN:
+                        fprintf(stdout,"%d\t",(int)col_value->value.asBoolean);
+                        break;
+                    case DPI_NATIVE_TYPE_BYTES:
+                        fprintf(stdout,"%.*s\t",col_value->value.asBytes.length,col_value->value.asBytes.ptr);
+                        break;
+                    case DPI_NATIVE_TYPE_DOUBLE:
+                        fprintf(stdout,"%.2f\t",col_value->value.asDouble);
+                        break;
+                    case DPI_NATIVE_TYPE_FLOAT:
+                        fprintf(stdout,"%.2f\t",col_value->value.asFloat);
+                        break;    
+                    case DPI_NATIVE_TYPE_INT64:
+                        fprintf(stdout,"%" PRIx64 "\t",col_value->value.asInt64);
+                        break;
+                    case DPI_NATIVE_TYPE_INTERVAL_DS:
+                        fprintf(stdout,"%" PRIx32 "|""%" PRIx32 "|""%" PRIx32 "|""%" PRIx32 "|""%" PRIx32 "\t",col_value->value.asIntervalDS.days,col_value->value.asIntervalDS.hours,col_value->value.asIntervalDS.minutes,col_value->value.asIntervalDS.seconds,col_value->value.asIntervalDS.fseconds);
+                        break;
+                    case DPI_NATIVE_TYPE_INTERVAL_YM:
+                        fprintf(stdout,"%" PRIx32 "|""%" PRIx32 "\t",col_value->value.asIntervalYM.months,col_value->value.asIntervalYM.years);
+                        break;
+                    case DPI_NATIVE_TYPE_NULL:
+                        break;
+                    case DPI_NATIVE_TYPE_TIMESTAMP:
+                        fprintf(stdout,"%" PRIu8 "|""%" PRIu8 "|""%d\t",col_value->value.asTimestamp.day,col_value->value.asTimestamp.month,col_value->value.asTimestamp.year);
+                        break;     
+                    default:
+                        break;
+                }            
+            }
         }
-        printf("\n");
+        if(FILE_FLAG){
+            fprintf(fp,"\n");
+        }
+        else fprintf(stdout,"\n");
         //fetching new row
         if (dpiStmt_fetch(statement, &found, &bufferRowIndex) < 0){
             printError(context,&error_info);
@@ -225,37 +287,68 @@ int statement_exec_routine(dpiConn *conn, dpiContext *context, char *SQL_string)
     return 0;
 }
 
+int statement_exec_wrapper(dpiConn *conn, dpiContext *context, char *user_SQL){
+    int choice=-1;
+    FILE *fp;
+    fprintf(stdout,"Do you wish to save query results to a file? [Y/N]\n~: ");
+    scanf("%lc", &choice);
+    while ((getchar()) != '\n');
+        if(choice==121 || choice==89){
+            time_t seconds;
+            seconds = time(NULL);
+            char *s=malloc(64*sizeof(char));
+            sprintf(s,"%ld-results.txt",seconds);
+            fprintf(stdout,"File opened: %s\n",s);
+            if((fp=fopen(s,"a+"))==NULL){
+                printf("[ERROR]\n\tAction: While opening new file.\n");
+                return -1;
+            }
+            if(statement_exec_routine(conn,context,user_SQL,TRUE,fp)<0){
+                printf("Unable to perform statement execution routine.\n");
+                return -1;
+            }
+            free(s);
+        }
+        else{
+            if(statement_exec_routine(conn,context,user_SQL,FALSE,NULL)<0){
+                printf("Unable to perform statement execution routine.\n");
+                return -1;
+            }
+        }
+    return 0;
+}
+
+// I/O automation function
 int import_from_file(dpiConn *conn, dpiContext *context){
     FILE *fp;
     char path[32], buffer[64], **SQL_string=malloc(sizeof(char));
-    int choice=-1,size=0, statement_counter=0;
+    int choice=-1, size=0, statement_counter=0;
     bool FLAG=TRUE, FLAG_1=FALSE;
 
     SQL_string[statement_counter]=malloc(sizeof(char));
-
+    
     fprintf(stdout,"________________________\n");
     fprintf(stdout,"|| Import statements  ||\n");
     fprintf(stdout,"‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\n");
     fprintf(stdout,"Do you wish to continue? [Enter 0 to return to main menu].\n~: ");
 
-    fscanf(stdin,"%d", &choice);
+    fscanf(stdin,"%lc", &choice);
     while ((getchar()) != '\n');
-
     if(choice==0) return 1;
     
     fprintf(stdout,"Insert the path to your file containing your SQL queries.\nIf the file is within the local directory, provide the name file only.\n~:");
-
-    fscanf(stdin,"%s",path);
-    while ((getchar()) != '\n');
     
+    fscanf(stdin,"%s",path);
+    
+    printf("you have inserted: %s\n",path);
+
     if((fp=fopen(path,"r"))==NULL){
         printf("[ERROR]\nFile does not exists/Wrong path.\n");
         return -1;
     }
-    
+    else printf("[OK]");
     //consuming buffer newline, because fscanf and fgets differences.
-    //while ((getchar()) != '\n');
-
+    
     while(fgets(buffer,(int)sizeof(buffer),fp)!=NULL){
         int i,j=0;
         //checking if this is the last line of a SQL statement
@@ -274,20 +367,21 @@ int import_from_file(dpiConn *conn, dpiContext *context){
                     }
                 }
                 if(occurrences%2!=0){
-                    //; is inside a string-> fake positive
+                    //; is inside a string-> fake positive. Resetting flag
                     FLAG_1=FALSE;
                 }
             }
         }
-
+        
         size=size+strlen(buffer);
-        if(FLAG_1){
-            //reached the end of the query, need to remove the ";"
-            //size of what to append and allocate changes accordingly
-            size--;
-            str_cut(buffer,j,1);
-        }
 
+        if(FLAG_1){
+            //reached the end of the query, need to remove the ";".
+            //size of what to append and allocate changes accordingly
+            str_cut(buffer,j-1,-1);
+            size--;
+        }
+        
         SQL_string[statement_counter]=realloc(SQL_string[statement_counter],size*sizeof(char));
         strcat(SQL_string[statement_counter],buffer);
 
@@ -301,6 +395,19 @@ int import_from_file(dpiConn *conn, dpiContext *context){
     }
     //I have all the statements, it's time to run them all.
 
+    for(int i=0;i<=statement_counter;i++){
+        if(statement_exec_wrapper(conn,context,SQL_string[i])<0){
+            printf("Unable to run Wrapper.\n");
+            return -1;
+        }
+    }
+
+    //free memory!
+    for(int i=0;i<=statement_counter;i++){
+        free(SQL_string[i]);
+    }
+    free(SQL_string);
+    
     return 0;
 }
 
@@ -313,6 +420,7 @@ int statement_creation(dpiConn *conn, dpiContext *context){
     int size=0;
     const char *string;
     dpiStmt *statement;
+    FILE *fp;
 
     fprintf(stdout,"________________________\n");
     fprintf(stdout,"|| Statement creation ||\n");
@@ -320,10 +428,10 @@ int statement_creation(dpiConn *conn, dpiContext *context){
     fprintf(stdout,"Do you wish to continue? [Enter 0 to return to main menu].\n~: ");
     fflush(stdout);
     fscanf(stdin,"%d", &choice);
-
+    fflush(stdin);
     if(choice==0) return 1;
 
-    fprintf(stdout,"Insert your SQL statement. Enter no characters to end the input.\n\n");
+    fprintf(stdout,"Insert your SQL ;-terminated statement.\n\n");
     fflush(stdout);
 
     //consuming buffer newline, because fscanf and fgets differences.
@@ -336,22 +444,28 @@ int statement_creation(dpiConn *conn, dpiContext *context){
             break;;
         }
         else{
-            if(buffer[0]=='\n'){
-                FLAG=FALSE;
+            //I want to stop when I see a ';'
+            for(int i=0;i<=strlen(buffer);i++){
+                if(buffer[i]==';'){
+                    FLAG=FALSE;
+                }
             }
-            else{
-                size=size+strlen(buffer);
-                user_SQL=realloc(user_SQL,size*sizeof(char));
-                strcat(user_SQL,buffer);
+            //';' must not be included buffer be like = {'D','A','T','A',';','\0'}
+            if(!FLAG){
+                size=size+strlen(buffer)-1;
+                str_cut(buffer,strlen(buffer)-2,-1);
             }
+            else  size=size+strlen(buffer);
+            
+            user_SQL=realloc(user_SQL,size*sizeof(char));
+            strcat(user_SQL,buffer);
         }
     }
-
-    if(statement_exec_routine(conn,context,user_SQL)<0){
-        printf("Unable to perform statement execution routine.\n");
+    
+    if(statement_exec_wrapper(conn,context,user_SQL)<0){
+        printf("Unable to run Wrapper.\n");
         return -1;
     }
-    
     free(user_SQL);
     
     return 0;
